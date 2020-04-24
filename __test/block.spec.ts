2cf224e3d47806ac5731cd8845d8570a08de2590
Max Medina <maxmedina05@gmail.com>
2020-04-24 08:09:42 +0200

    implement validate and getBData for blocks

---
+++ __test/block.spec.ts
@@ -0,0 +1,47 @@
+const SHA256 = require('crypto-js/sha256');
+const Block = require("../src/block").Block
+
+function generateHash(obj) {
+    return SHA256(JSON.stringify(obj)).toString()
+}
+
+describe('Block Class Test', () => {
+    test('validate should return false if block has been tampered', () => {
+        const data = {
+            text: 'random text'
+        }
+
+        const block = new Block(data)
+        block.body = {
+            message: 'I was here!'
+        }
+
+        return block.validate().then((isValid) => {
+            expect(isValid).toEqual(false)
+        })
+    })
+
+    test('getBData should return the block body decoded', () => {
+        expect.assertions(1);
+        const expectedData = {
+            text: 'random text'
+        }
+
+        const block = new Block(expectedData)
+
+        return block.getBData().then(result => {
+            expect(result).toEqual(expectedData)
+        })
+    })
+
+    test('getBData should throw when trying to read the data for the genesis block', () => {
+        expect.assertions(1);
+        const block = new Block({ data: 'Genesis Block' })
+
+        return block.getBData().catch(error => {
+            expect(error).toEqual({
+                error: 'Data could not be retrieve!'
+            })
+        })
+    })
+})
